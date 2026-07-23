bcfff825571dbca22dcd30d3e1db629eb8c22504
otavioou <otaviooliveiradasilva788@gmail.com>
2026-07-22 21:31:49 -0300

    fazendo tela home

--- Frontend/src/pages/Home.tsx
+++ Frontend/src/pages/Home.tsx
@@ -1,4 +1,5 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonContent, IonHeader, IonIcon, IonImg, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { chevronDown } from 'ionicons/icons';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
@@ -6,17 +7,31 @@ const Home: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+        <IonToolbar className="home-toolbar">
+          <IonTitle className="home-title">TaskMaster</IonTitle>
+          <div className="home-actions" slot="end">
+            <IonButton href="/login">Login</IonButton>
+            <IonButton href="/cadastro">Cadastro</IonButton>
+          </div>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
+        <p className="home-description">
+          Gerenciador de Tarefas
+        </p>
+
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            <IonTitle size="large">TaskMaster</IonTitle>
           </IonToolbar>
         </IonHeader>
         <ExploreContainer />
+        <div className="task-button-container">
+          <IonButton className="task-button-new" href="/nova-tarefa">
+            Nova tarefa
+            <IonIcon icon={chevronDown} slot="end" />
+          </IonButton>
+        </div>
       </IonContent>
     </IonPage>
   );
